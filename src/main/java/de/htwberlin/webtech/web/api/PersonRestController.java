f236ccc86a278ae1b461ad34e469870efc5ca86e
TunaKadirBerkay <Kadir.Tuna@Student.HTW-Berlin.de>
2023-11-23 18:22:43 +0100

    PostgreSQL JDBC Driver  und Spring Boot Starter Data JPA

---
+++ src/main/java/de/htwberlin/webtech/web/api/PersonRestController.java
@@ -0,0 +1,30 @@
+package de.htwberlin.webtech.web;
+
+
+import de.htwberlin.webtech.web.api.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PersonRestController {
+    private List<Person> persons;
+
+
+    public PersonRestController(){
+        persons = new ArrayList<>();
+        persons.add(new Person(1,"Max","Mustermann",false));
+        persons.add(new Person(1,"Maxima","Meier",true));
+    }
+    @GetMapping(path = "/api/v1/persons")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Person>fetchPerson() {
+        return persons;
+
+    }
+}
